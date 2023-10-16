6853d002d57f95f446ae5063d763eaf9d2790286
Yashika-Sharma03 <sharma.yashika1903@gmail.com>
2023-10-17 02:07:49 +0530

    Added a program which uses throw keyword

--- ExceptionHandling1.java
+++ ExceptionHandling1.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
 public class ExceptionHandling1 {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter numerator");
-        int num = sc.nextInt();
-        System.out.println("Enter denominator");
-        int den = sc.nextInt();
 
-        try{
+    public static void main(String args[]){
+        try(Scanner sc = new Scanner(System.in);){
+            System.out.println("Enter numerator");
+            int num = sc.nextInt();
+            System.out.println("Enter denominator");
+            int den = sc.nextInt();
             int res = num/den;
             System.out.println("Result is : " + res);
         }
